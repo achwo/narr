60d9684d3e49bf61fe29afd5f92627eac85b419b
Felix Jensen <felix.jensen@mindmatters.de>
2024-11-20 17:22:26 +0100

    Don't replace ' in file paths

--- utils/strings_test.go
+++ utils/strings_test.go
@@ -56,6 +56,7 @@ func TestSanitizePathComponent(t *testing.T) {
 	}{
 		{name: "does not replace .", input: "J.R.R. Tolkien", expected: "J.R.R. Tolkien"},
 		{name: "does not replace ?", input: "Something With ???", expected: "Something With ???"},
+		{name: "does not replace '", input: "Stanislawa d'Asp", expected: "Stanislawa d'Asp"},
 	}
 
 	for _, tt := range tests {
